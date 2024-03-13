83bf7d5ba902ece132b63f687125e6a116c089d7
NathanielBrewer <nathanielc.brewer@gmail.com>
2024-03-13 14:23:15 -0300

    feat: add dotenv and firebase

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,26 @@
 import fastify, { FastifyInstance } from 'fastify';
 import { Server, IncomingMessage, ServerResponse } from 'http'
+import * as fbAdmin from 'firebase-admin';
+import * as dotenv from 'dotenv';
 
-const server: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify(
-  {
-    logger: {
-      name: __filename,
-      level: 'info',
-    },
-  })
+dotenv.config();
 
-const start = async () => {
-    await server.listen({port: 3000}, (error: Error | null, address: string | number) => {
-      if (error) {
-        server.log.error(error);
-      }
-      server.log.info(`Server listening on ${address}`);
-    });
-};
-start();
+const fbApp: fbAdmin.app.App = fbAdmin.initializeApp({
+  credential: fbAdmin.credential.cert(process.env.GOOGLE_APPLICATION_CREDENTIALS as string)
+});
+
+const fbFirestore: fbAdmin.firestore.Firestore = fbApp.firestore();
+
+const server: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({
+  logger: {
+    name: __filename,
+    level: 'info',
+  },
+});
+
+server.listen({port: Number(process.env.PORT) ?? 3000}, (error: Error | null, address: string | number) => {
+  if (error) {
+    server.log.error(error);
+  }
+  server.log.info(`Server listening on ${address}`);
+});

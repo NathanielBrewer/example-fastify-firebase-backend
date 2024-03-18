8b83cac573b56895bc8797f00c26c497d0085eac
NathanielBrewer <nathanielc.brewer@gmail.com>
2024-03-18 14:10:55 -0300

    feat: add firebase bucket for storage

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,14 +5,17 @@ dotenv.config();
 
 if (fbAdmin.apps.length === 0) {
   fbAdmin.initializeApp({
-    credential: fbAdmin.credential.cert(process.env.GOOGLE_APPLICATION_CREDENTIALS as string)
+    credential: fbAdmin.credential.cert(process.env.GOOGLE_APPLICATION_CREDENTIALS as string),
+    storageBucket: 'gs://parados-app.appspot.com',
   });
 }
 
 const db: fbAdmin.firestore.Firestore = fbAdmin.apps[0]!.firestore();
 const textCollection = db.collection('texts');
+const bucket = fbAdmin.storage().bucket();
 
 export { 
   db,
   textCollection,
+  bucket,
 }
